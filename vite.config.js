ca0d3c3c8aa797d829769412d92a17fc81b1768e
cudavailable <1598232230@qq.com>
2024-11-24 19:07:06 +0800

    test 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 按需加载
+// import styleImport from 'vite-plugin-style-import'
+// import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: "/GDP-Data-Visualization/",
+  build: {
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+        }
+    }
+  }
 })
+
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+  // 
+  // 
+// })
