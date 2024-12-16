fbc868594385a2f91c5924ab46d421866a966b29
cudavailable <1598232230@qq.com>
2024-12-16 14:00:03 +0800

    倒回上一状态

--- src/views/EconomyOverview.vue
+++ src/views/EconomyOverview.vue
@@ -140,10 +140,9 @@ export default {
         return;
       }
 
-      const provinceMapFile = `/assets/${fileName}.json`;
       // const provinceMapFile = `/assets/provinces/${fileName}.json`;
-      // const baseUrl = import.meta.env.BASE_URL || '/';
-      // const provinceMapFile = `${baseUrl}assets/${fileName}.json`;  
+      const baseUrl = import.meta.env.BASE_URL || '/';
+      const provinceMapFile = `${baseUrl}assets/provinces/${fileName}.json`;  
       fetch(provinceMapFile)
         .then((response) => {
           if (!response.ok) throw new Error(`文件 ${fileName} 不存在`);
